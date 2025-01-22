e1818bc982eeca509051491dbe54fabcb016f971
Anees Omar <aneesomar.ao@gmail.com>
2025-01-22 16:35:38 +0200

    reads resturants from a JSON file instead of from code this will be used later on for database purposes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,69 +2,19 @@ import React from 'react';
 import Tile from '../components/Tile';
 import '../app/globals.css';
 import OpenStreetMap from '../components/OpenStreetMap';
+import restaurants from '../data/rest.json';
 
 
 
 export default function Home() {
-    const restaurants = [
-        {
-            name: 'Haiku Restaurant',
-            description: 'The best Asian restaurant with sushi, dim sum...',
-            distance: 0.6,
-            reviews: 584,
-            image: '/images/sushi.jpg',
-            rating: 4.5,
-        },
-        {
-            name: 'Hacienda Coastal Mexican',
-            description: 'An extraordinary dining experience...',
-            distance: 0.4,
-            reviews: 107,
-            image: '/images/paneer-tikka.jpg',
-            rating: 4.1,
-        },
-        {
-            name: 'Mantra Cafe',
-            description: 'Delicious mussels and tuna ceviche...',
-            distance: 5.3,
-            reviews: 950,
-            image: '/images/the-magic-of-mantra.jpg',
-            rating: 4.8,
-        },
-        {
-            name: 'Haiku Restaurant',
-            description: 'The best Asian restaurant with sushi, dim sum...',
-            distance: 0.45,
-            reviews: 584,
-            image: '/images/sushi.jpg',
-            rating: 5,
-        },
-        {
-            name: 'Hacienda Coastal Mexican',
-            description: 'An extraordinary dining experience...',
-            distance: 0.4,
-            reviews: 107,
-            image: '/images/paneer-tikka.jpg',
-            rating: 4.1,
-        },
-        {
-            name: 'Mantra Cafe',
-            description: 'Delicious mussels and tuna ceviche...',
-            distance: 5.3,
-            reviews: 950,
-            image: '/images/the-magic-of-mantra.jpg',
-            rating: 4.8,
-        },
-    ];
-    //sort resturants by distance
+    //sort restaurants by distance
     const sortedRestaurants = restaurants.sort((a, b) => (b.rating / b.distance) - (a.rating / a.distance));
 
     return (
         <div className="p-8 space-y-4">
-
             <h1 className='title'>Rest Halaal</h1>
             <OpenStreetMap />
-            {restaurants.map((restaurant, index) => (
+            {sortedRestaurants.map((restaurant, index) => (
                 <Tile
                     key={index}
                     name={restaurant.name}
@@ -78,6 +28,5 @@ export default function Home() {
         </div>
     );
 };
-
 
 
